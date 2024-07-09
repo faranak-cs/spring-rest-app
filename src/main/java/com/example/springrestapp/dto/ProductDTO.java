8b95aae7cb1ba0c55c902148e057735d397e4679
faran khan <fakhan@beyond.com>
2024-07-09 12:26:04 +0100

    mapper class ✅

--- src/main/java/com/example/springrestapp/dto/ProductDTO.java
+++ src/main/java/com/example/springrestapp/dto/ProductDTO.java
@@ -1,6 +1,14 @@
 package com.example.springrestapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ProductDTO {
+    @JsonProperty("ID")
     private Integer productId;
+    @JsonProperty("PRODUCT_NAME")
     private String productName;
 }
--- src/main/java/com/example/springrestapp/mapper/ProductMapper.java
+++ src/main/java/com/example/springrestapp/mapper/ProductMapper.java
@@ -1,9 +1,31 @@
 package com.example.springrestapp.mapper;
 
+import com.example.springrestapp.dto.ProductDTO;
+import com.example.springrestapp.model.ProductModel;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductMapper {
 
     // MAPS FROM DTO TO ENTITY, AND VICE VERSA
+
+
+    public static ProductModel toModel(ProductDTO productDTO){
+
+        ProductModel productModel = new ProductModel();
+        productModel.setProductId(productDTO.getProductId());
+        productModel.setProductName(productDTO.getProductName());
+
+        return productModel;
+    }
+
+    public static ProductDTO toDTO(ProductModel productModel){
+
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductId(productModel.getProductId());
+        productDTO.setProductName(productModel.getProductName());
+
+        return productDTO;
+    }
+
 }
