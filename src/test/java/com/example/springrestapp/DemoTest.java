d79598c13965eb1aacff9646a69d3d5a7a8c1752
faran khan <fakhan@beyond.com>
2024-07-15 12:23:43 +0100

    added test files

---
+++ src/test/java/com/example/springrestapp/DemoTest.java
@@ -0,0 +1,55 @@
+package com.example.springrestapp;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.assertj.core.api.Fail.fail;
+
+
+public class DemoTest {
+
+    @BeforeAll
+    static void initAll(){
+        // SERVICES
+        // IT COULD BE DOCKER IMAGES THAT MUST BE RUNNING PRIOR TO RUNNING TESTS
+    }
+
+    @BeforeEach
+    void init(){
+        // ANY SPECIFIC SERVICE YOU WANT TO BE RUNNING BEFORE TESTS
+    }
+
+    @Test
+    public void succeedingTest(){
+        // ACTUAL TEST
+    }
+
+    @Test
+    public void failingTest(){
+        // FAIL TEST
+        fail("A failing test");
+    }
+
+    @Test
+    @Disabled("for demo")
+    public void skippedTest(){
+        // WILL NOT BE EXECUTED
+    }
+
+    @Test
+    public void abortedTest(){
+        assumeTrue("abc".contains("Z"));
+        fail("test should have been aborted");
+    }
+
+    @AfterEach
+    void tearDown(){
+        // CLOSES SERVICE
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        // CLOSES ALL THE SERVICES
+    }
+
+}
---
+++ src/test/java/com/example/springrestapp/controller/ProductControllerTest.java
@@ -0,0 +1,4 @@
+package com.example.springrestapp.controller;
+
+public class ProductControllerTest {
+}
